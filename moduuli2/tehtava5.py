2e48b24b4c23bc8b8185ced65f140a7e4bd52db7
Def-fect <maxrah@metropolia.fi>
2025-01-17 15:08:48 +0200

    tehtavat moduuli2

--- moduuli2/tehtava5.py
+++ moduuli2/tehtava5.py
@@ -1,21 +1,12 @@
 luoti, naula, leiviskä = float(input("Anna luotien määrä: ")), float(input("Anna naulojen määrä: ")), float(input("Anna leiviskien määrä: "))
 
-
-
-
-
 luoti_g = (13.3)
 naula_g = (32 * luoti_g)
 leiviskä_g = (20 * naula_g)
 
-tulos1 = (luoti * luoti_g)
-tulos2 = (naula * naula_g)
-tulos3 = (leiviskä * leiviskä_g)
-
-tuloksientulos = (tulos3 + tulos1 + tulos2)
+massa_grammoina = leiviskä_g * leiviskä + naula_g * naula + luoti_g * luoti
 luku1 = 1000
 
-print(luoti_g)
-tulos = (tuloksientulos % luku1)
-tulos4 = (tuloksientulos // 1000)
-print(f"kiloina {(tulos4):.0f}",(f"ja grammoina {(tulos):.0f}"))
+tulos = (massa_grammoina % luku1)
+tulos4 = (massa_grammoina // 1000)
+print(f"kiloina {(tulos4):.0f}",(f"ja grammoina {(tulos):.2f}"))
