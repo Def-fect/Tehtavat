a534128fea42bd5e98845f2e733e785b5d8f804f
Def-fect <maxrah@metropolia.fi>
2025-01-18 16:01:04 +0200

    tehtavat moduuli2

--- moduuli2/tehtava3.py
+++ moduuli2/tehtava3.py
@@ -9,5 +9,10 @@ korkeus =float(korkeus)
 piiri = int((korkeus * 2) +(kanta * 2))
 
 pintaala = int(korkeus * kanta)
+
+piiri = ((korkeus * 2) +(kanta * 2))
+
+pintaala = (korkeus * kanta)
+
 print("suorakulmion pinta-ala on: " + str(piiri))
 print("suorakulmion pinta-ala on: " + str(pintaala))
