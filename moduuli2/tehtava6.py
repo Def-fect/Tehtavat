80d40b7af637ce0ec113fdca1f941d437894cc34
Def-fect <maxrah@metropolia.fi>
2025-01-18 16:03:33 +0200

    tehtavat moduuli2

--- moduuli2/tehtava6.py
+++ moduuli2/tehtava6.py
@@ -1,5 +1,5 @@
 import random
 
-print("kolmenumeroinen koodi "+str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) )
+print("Kolmenumeroinen koodi "+str(random.randint(0, 9)) + str(random.randint(0, 9)) + str(random.randint(0, 9)) )
 
-print("neljänumeroinen koodi "+str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)))
+print("Neljänumeroinen koodi "+str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)) + str(random.randint(1, 6)))
