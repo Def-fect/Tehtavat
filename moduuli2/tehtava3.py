c71074e30e36796991d24aa37cdbdceec756a6e5
Def-fect <maxrah@metropolia.fi>
2025-01-18 16:02:27 +0200

    tehtavat moduuli2

--- moduuli2/tehtava3.py
+++ moduuli2/tehtava3.py
@@ -1,2 +1,13 @@
-käyttäjä = input("Anna nimi")
-print("Terve" + käyttäjä + "!")
+import math
+
+kanta = input("mikä on kannan pituus?: ")
+korkeus = input("mikä on korkeus?: ")
+
+kanta=float(kanta)
+korkeus =float(korkeus)
+
+piiri = int((korkeus * 2) +(kanta * 2))
+
+pintaala = int(korkeus * kanta)
+print("suorakulmion pinta-ala on: " + str(piiri))
+print("suorakulmion pinta-ala on: " + str(pintaala))
