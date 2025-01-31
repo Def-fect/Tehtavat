16aef2facb2a7f8e408a57fba5a53b168a2535ef
Def-fect <maxrah@metropolia.fi>
2025-01-31 12:02:08 +0200

    tehtava moduuli5

--- moduuli5/tehtava3.py
+++ moduuli5/tehtava3.py
@@ -1,17 +1,12 @@
 
-num = int(input("put numba here"))
-# Negative numbers, 0 and 1 are not primes
+num = int(input("Anna luku"))
+
 if num > 1:
-
-    # Iterate from 2 to n // 2
     for i in range(2, (num // 2) + 1):
-
-        # If num is divisible by any number between
-        # 2 and n / 2, it is not prime
         if (num % i) == 0:
-            print(num, "is not a prime number")
+            print(num, "ei ole alkuluku")
             break
     else:
-        print(num, "is a prime number")
+        print(num, "luku on alkuuku")
 else:
-    print(num, "is not a prime number")
+    print(num, "ei ole alkuluku")
---
+++ moduuli5/tehtava4.py
@@ -0,0 +1,8 @@
+kaupungit = []
+
+for i in range(5):
+    kaupunki=input("syötä 5 kaupunkia: ")
+    kaupungit.append(kaupunki)
+for i in kaupunki[5]:
+    print(kaupungit)
+
