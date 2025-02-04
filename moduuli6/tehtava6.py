da646fb9a41e8be90775ba692693f2bc4a1f6677
Def-fect <maxrah@metropolia.fi>
2025-02-04 13:03:12 +0200

    tehtava moduuli6

--- moduuli6/tehtava6.py
+++ moduuli6/tehtava6.py
@@ -3,6 +3,19 @@ def pizza1():
     eurohinta=float(input("Anna ensimmäisen pizzan hinta"))
     halkaisija = float(input("Anna ensimmäisen pizzan halkaisija"))
     neliom=(halkaisija**2*(math.pi/4))/1000
-    print(neliom)
-    return
-print(pizza1())
+    x=neliom/eurohinta
+    return x
+def pizza2():
+    eurohinta2=float(input("Anna toisen pizzan hinta"))
+    halkaisija2 = float(input("Anna toisen pizzan halkaisija"))
+    neliom2=(halkaisija2**2*(math.pi/4))/1000
+    x2=neliom2/eurohinta2
+    return x2
+
+pizza1=pizza1()
+pizza2=pizza2()
+
+if pizza2<pizza1:
+    print("ensimmäinen pizza on edullisempi",pizza1)
+else:
+    print("toinen pizza on edullisempi", pizza2)
