e18d28f31b7ffdb2050d2289e4196613af1c5a35
Def-fect <maxrah@metropolia.fi>
2025-04-13 14:27:42 +0300

    tehtavat moduuli 8

--- moduuli8/tehtava1.py
+++ moduuli8/tehtava1.py
@@ -1,8 +1,5 @@
 import pymysql
 
-
-
-
 yhteys = pymysql.connect(
     host='localhost',
     port=3306,
@@ -10,16 +7,14 @@ yhteys = pymysql.connect(
     user='deffect',
     password='qwerty',
     autocommit=True,
-
 )
 x=input("Anna koodi")
 
 sql = (f"select country.name, airport.name "
        f"from airport, country "
        f"where airport.iso_country=country.iso_country and airport.ident='{x}';"
-
 )
-print(sql)
+
 kursori = yhteys.cursor()
 kursori.execute(sql)
 tulos = kursori.fetchall()
